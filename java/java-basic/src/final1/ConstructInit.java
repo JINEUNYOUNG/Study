cf698e2564ccfcb88329d773ed244d338236df0c
jineunyoung <apages@naver.com>
2024-01-01 16:24:14 +0900

    .

---
+++ java/java-basic/src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;
+    } //이때, 생성자를 통해서만 값을 할당 가능!
+}
--- java/java-basic/src/final1/FinalLocalMain.java
+++ java/java-basic/src/final1/FinalLocalMain.java
@@ -7,6 +7,13 @@ public class FinalLocalMain {
         data1 = 10; //최초 한번만 할당
         //data2 = 20; <-이게안됨
 
+        final int data2 = 10;
+        //data2 = 20; 물론안됨
+        method(10);
 
+
+    }
+    static void method(final int a){
+        //a = 20; 이럼 안됨..
     }
 }

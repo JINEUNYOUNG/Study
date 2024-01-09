984207ca1d55ac7f3be8041e78c658209ebd77cd
jineunyoung <apages@naver.com>
2024-01-10 03:30:14 +0900

    spring

--- spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,10 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -20,10 +22,9 @@ public class MemberService {
      *  회원 가입
      */
     public Long join(Member member){
-        //중복 회원은 안된다.
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,8 +37,9 @@ public class MemberService {
     /**
      *  전체 회원 조회
      */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+    public List<Member> findMembers() {
+            return memberRepository.findAll();
+
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
--- spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,34 +1,43 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    //@PersistenceContext 원랜 해줘야함.
+
+    /*  private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
+    }*/
 
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
 
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    }*/
+
 }
